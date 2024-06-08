626c3830ef8a82fb05c247b00404addc437f4065
Narlan Mota <narlanmota19@gmail.com>
2024-06-08 14:00:04 -0300

    Projeto vazio configurado

---
+++ src/main/java/com/backend/backpw/BackpwApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backpw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/backpw/BackpwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backpw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
